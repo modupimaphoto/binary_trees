d513830340a90b60a5a9a9d32bc80d65a6ad6f0c
Modupi <modupimaphoto@gmail.com>
2024-02-23 01:17:05 +0200

    4. Is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ *
+ * @node: a pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	int is_leaf;
+
+	is_leaf = (!node || node->left || node->right) ? 0 : 1;
+
+	return (is_leaf);
+}
